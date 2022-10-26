517ee9403aa1067c3d541ab855b2155efdf88729
Farid Hernandez <farid.hdz.64@gmail.com>
2022-10-25 20:35:42 -0600

    test: Example non-related test

---
+++ src/Example.tsx
@@ -0,0 +1,45 @@
+const Example = () => null;
+// <form action="">
+//   <input
+//     type="text"
+//     name="name"
+//     id="name"
+//     value={state.name}
+//     onChange={handleOnChange}
+//     className="form-control form-input"
+//   />
+//   <input
+//     type="text"
+//     name="email"
+//     id="email"
+//     value={state.email}
+//     onChange={handleOnChange}
+//     className="form-control form-input"
+//   />
+//   <input
+//     type="text"
+//     name="username"
+//     id="username"
+//     value={state.username}
+//     onChange={handleOnChange}
+//     className="form-control form-input"
+//   />
+//   <input
+//     type="text"
+//     name="password"
+//     id="password"
+//     value={state.password}
+//     onChange={handleOnChange}
+//     className="form-control form-input"
+//   />
+//   <input
+//     type="text"
+//     name="confirmPassword"
+//     id="confirmPassword"
+//     value={state.confirmPassword}
+//     onChange={handleOnChange}
+//     className="form-control form-input"
+//   />
+// </form>
+
+export default Example;
